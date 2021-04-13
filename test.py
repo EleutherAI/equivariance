f282cf5e202915d0b08496647c2612b8d44de0b4
Ryan Teehan <rsteehan@gmail.com>
2021-04-13 17:38:20 -0400

    add application test

--- test.py
+++ test.py
@@ -4,3 +4,16 @@ from utils import AffineMap
 '''
 Affine Map Tests
 '''
+def test_apply():
+    '''
+    Tests applying the affine map to a point
+    :return:
+    '''
+    A = np.array([[1,0],[0,1]])
+    t = np.array([5,5])
+    x = np.array([1,2])
+    map = AffineMap(A, t)
+    output = map.apply(x)
+    assert output[0] == 6 and output[1] == 7, "Pure translation map must apply correctly"
+
+
